49e546edee9aa41d090be7cd267a629628346d52
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-10-07 22:34:06 +0530

    Refactor customErr.js to add ApiError class with extended functionality

--- 20_OOPs-2/03_ER/customErr.js
+++ 20_OOPs-2/03_ER/customErr.js
@@ -1,1 +1,26 @@
-// Custom ApiError and Custom ApiResponse 
+// Custom ApiError and Custom ApiResponse
+
+class ApiError extends Error {
+
+    constructor(
+        statusCode,
+        message = "something went wrong",
+        error = [], // A list of additional errors
+        stack = ""  // A stack trace (a detailed error location)
+    ){
+
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.error = error
+
+        if (stack) {
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+    }
+}
