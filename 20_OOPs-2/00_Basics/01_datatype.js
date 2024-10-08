f16718ac307e29866ce7c43b5c85fa655e18a37d
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-10-08 13:35:26 +0530

    Add datatype.js with examples of different data types and printing methods

---
+++ 20_OOPs-2/00_Basics/01_datatype.js
@@ -0,0 +1,34 @@
+//TODO: Printing
+
+// console.log("Hello")
+// process.stdout.write("World")
+// console.table({city: "Jaipur"})
+
+
+//TODO: Datatype
+
+/*
+String
+Number
+Boolean
+Bigint
+
+undefined
+null
+
+Array
+object
+symbol
+*/
+
+let score = 102
+let gmail = "abhi@gmail.com"
+let isLoggedIn = false
+
+
+// Array & Object
+let teaTypes = ["lemonTea", "orangeTea", "masalaTea"]
+let user = {firstname: "John", lastname: "Doe"}
+
+
+let getScore = score
