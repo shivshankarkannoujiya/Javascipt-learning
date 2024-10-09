2ab9e06050a52baaeb04e824616758ee6cdcd7a0
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-10-09 21:08:56 +0530

    Add conditional challenge

---
+++ 20_OOPs-2/00_Basics/Part-3/conditionalChallenges.js
@@ -0,0 +1,10 @@
+// Checking if a number is greater than another number 
+
+let num1 = 5
+let num2 = 8
+
+if (num1 > num2) {
+    console.log("num1 is greater than num2")
+}else{
+    console.log("Nope, num1 is not greater")
+}
