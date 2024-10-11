b85dade6d1ce41893d75f0c0fbfd17d241a28d89
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-10-11 12:00:00 +0530

    Add conditional challenge

--- 20_OOPs-2/00_Basics/Part-3/conditionalChallenges.js
+++ 20_OOPs-2/00_Basics/Part-3/conditionalChallenges.js
@@ -3,8 +3,63 @@
 let num1 = 5
 let num2 = 8
 
+/*
 if (num1 > num2) {
     console.log("num1 is greater than num2")
 }else{
     console.log("Nope, num1 is not greater")
+}
+
+*/
+
+// Check if a string is equal to another string
+
+let username = "abhi"
+let anotherUsername = "abhi"
+
+/*
+if (username != anotherUsername) {
+    console.log("Pick another username")
+}else{
+    console.log("You can pick this username")
+}
+*/
+
+
+//Check if a variable is number or not
+
+let score = 40
+
+/*
+if (typeof score === 'number') {
+    console.log("Yep, this is a number")
+}else{
+    console.log("No, that is not a number")
+}
+*/
+
+
+
+// Checking if a boolean value is true or false
+
+let isTeaReady = false
+
+/*
+if (isTeaReady) {
+    console.log("Tea is Ready")
+}else{
+    console.log("Tea is not Ready")
+}
+*/
+
+
+// Checking if an Array is empty or not 
+
+let items = []
+console.log(items.length)
+
+if (items.length === 0) {
+    console.log("Array is empty")
+}else{
+    console.log("Array is NOT empty")
 }
